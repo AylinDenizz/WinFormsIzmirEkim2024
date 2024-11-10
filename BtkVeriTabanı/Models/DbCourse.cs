5d6fd54c226787799db2bd2e71ed996283b6e831
Aylin Deniz <aylinozunlu@gmail.com>
2024-11-10 10:03:06 +0300

    DbCourse.cs, DbInstructor entities created

---
+++ BtkVeriTabanı/Models/DbCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkVeriTabanı.Models
+{
+    internal class DbCourse
+    {
+        public  int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public  DateTime CreatedDate { get; set; }
+        public int LocatedCity {  get; set; }
+        public int  LocatedDistrict {  get; set; }
+        public string CourseLocation {  get; set; }
+        public int CourseInstructor { get; set; }
+
+        public DbCourse() { }
+    }
+
+
+}
---
+++ BtkVeriTabanı/Models/DbInstroctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkVeriTabanı.Models
+{
+    internal class DbInstroctor
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DbInstroctor() { }
+    }
+}
